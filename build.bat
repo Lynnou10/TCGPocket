f91c496f7c6a7911f561b1f1f843f77739f0234e
Nicolas Cluchet <nicolas.cluchet@yahoo.fr>
2025-05-23 23:08:37 +0200

    Fix build + font

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ pyinstaller --onefile --icon=favicon.ico -n PTCGTracker server.py
 xcopy cards dist\cards /s /i
 xcopy utils dist\utils /s /i
 xcopy favicon.ico dist\favicon.ico*
-xcopy index.html dist\index.html.ico*
+xcopy index.html dist\index.html*
 xcopy styles.css dist\styles.css*
 rmdir /s /q "./build"
 del "./PTCGTracker.spec"
