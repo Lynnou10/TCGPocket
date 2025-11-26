6a4bbe57bdf50c9909494b59a87149e8d9d9410b
Nicolas Cluchet <nicolas.cluchet@yahoo.fr>
2025-11-26 12:12:06 +0100

    Fix build

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ del "./PTCGTracker.spec"
 pyinstaller --onefile --icon=favicon.ico -n PTCGTracker server.py
 xcopy cards dist\cards /s /i
 xcopy utils dist\utils /s /i
+xcopy img dist\img /s /i
 xcopy favicon.ico dist\favicon.ico*
 xcopy index.html dist\index.html*
 xcopy app.html dist\app.html*
